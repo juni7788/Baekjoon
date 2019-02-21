12570a63fb5f508867ddd27078ae4aa1bc9d75fd
juni7788 <eui945@naver.com>
2019-02-21 11:30:09 +0900

    삼성 기출. BFS

---
+++ 16234.java
@@ -0,0 +1,113 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 16234 {
+
+	static int N, L, R, count, limit, part;
+	static int [] people;
+	static int [][] union, map;
+	static int [][] move = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	static Pairlist [] list;
+	static Queue <Pair> queue;
+			
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner (System.in);
+		N = scanner.nextInt();
+		L = scanner.nextInt();
+		R = scanner.nextInt();
+		map = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				map[i][j] = scanner.nextInt();
+		}
+		
+		for (; ;count++) {
+			set_default();
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (union[i][j] != 0)
+						continue;
+					set_start (i, j, ++part);
+					
+					while (!queue.isEmpty()) {
+						Pair pair = queue.poll();
+						int now_x = pair.x;
+						int now_y = pair.y;
+						
+						for (int k = 0; k < 4; k++) {
+							int x = now_x + move[k][0];
+							int y = now_y + move[k][1];
+							
+							if (0 > x || x >= N || 0 > y || y >= N)
+								continue;
+							if (union[x][y] != 0)
+								continue;
+							
+							int diff = Math.abs(map[x][y] - map[now_x][now_y]);
+							if (L <= diff && diff <= R) {
+								union[x][y] = part;
+								queue.offer(new Pair (x, y));
+								list[part].offer(new Pair(x, y));
+								people[part] += map[x][y];
+							}
+						}
+					}
+				}
+			}
+
+			if (part == N*N) // There is no union
+				break;
+			
+			for (int i = 1; i <= limit; i++) {
+				int num = list[i].size();
+				if (num == 1)
+					continue;
+				int avg = people[i]/num;
+				
+				while (!list[i].isEmpty()) {
+					Pair pair = list[i].poll();
+					int x = pair.x;
+					int y = pair.y;
+					
+					map[x][y] = avg;
+				}
+			}
+		}
+		
+		System.out.println(count);
+		scanner.close();
+	}
+	
+	static void set_default() {
+		list = new Pairlist [N*N+2];
+		people = new int [N*N+2];
+		union = new int [N][N];
+		
+		for (int i = 0; i < N*N+2; i++)
+			list[i] = new Pairlist ();
+	}
+	
+	static void set_start(int x, int y, int num) {
+		queue = new LinkedList <Pair> ();
+		queue.offer(new Pair (x, y));
+		people[num] = map[x][y];
+		list[num].add(new Pair (x, y));
+		union[x][y] = num;
+		limit = num;
+		part = 0;
+	}
+}
+
+class Pair {
+	int x, y;
+	
+	Pair (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Pairlist extends LinkedList <Pair> {}
